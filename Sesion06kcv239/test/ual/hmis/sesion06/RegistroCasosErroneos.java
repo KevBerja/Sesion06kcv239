eaf4b6017cc004f4300cb1a3ca0d357c928b5d10
Kevin Carracedo Vázquez <kevirgitano@gmail.com>
2018-06-05 19:08:46 +0200

    Create Maven Project and insert into it the JUnit files of tests from the Session06

---
+++ Sesion06kcv239/test/ual/hmis/sesion06/RegistroCasosErroneos.java
@@ -0,0 +1,138 @@
+package ual.hmis.sesion06;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class RegistroCasosErroneos {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "https://www.katalon.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testRegistroCasosErroneos() throws Exception {
+    driver.get("https://logappf1hmis2018.azurewebsites.net/");
+    driver.findElement(By.linkText("Register")).click();
+    driver.findElement(By.id("Email")).click();
+    driver.findElement(By.id("Email")).clear();
+    driver.findElement(By.id("Email")).sendKeys("prueba123@dominio.uig");
+    driver.findElement(By.id("Password")).click();
+    driver.findElement(By.id("Password")).clear();
+    driver.findElement(By.id("Password")).sendKeys("franklinychop");
+    driver.findElement(By.id("ConfirmPassword")).click();
+    driver.findElement(By.id("ConfirmPassword")).clear();
+    driver.findElement(By.id("ConfirmPassword")).sendKeys("franklinychop");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    driver.findElement(By.linkText("Register")).click();
+    driver.findElement(By.id("Email")).click();
+    driver.findElement(By.id("Email")).clear();
+    driver.findElement(By.id("Email")).sendKeys("hola@ual.es");
+    driver.findElement(By.id("Password")).click();
+    driver.findElement(By.id("Password")).clear();
+    driver.findElement(By.id("Password")).sendKeys("ABab12!!");
+    driver.findElement(By.id("ConfirmPassword")).click();
+    driver.findElement(By.id("ConfirmPassword")).click();
+    driver.findElement(By.id("ConfirmPassword")).clear();
+    driver.findElement(By.id("ConfirmPassword")).sendKeys("ABab12!!");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    driver.findElement(By.linkText("Register")).click();
+    driver.findElement(By.id("Email")).click();
+    driver.findElement(By.id("Email")).clear();
+    driver.findElement(By.id("Email")).sendKeys("prueba123@dominio.uig");
+    driver.findElement(By.id("Password")).clear();
+    driver.findElement(By.id("Password")).sendKeys("Prueba123");
+    driver.findElement(By.id("ConfirmPassword")).click();
+    driver.findElement(By.id("ConfirmPassword")).clear();
+    driver.findElement(By.id("ConfirmPassword")).sendKeys("Prueba123");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    driver.findElement(By.linkText("Register")).click();
+    driver.findElement(By.id("Email")).click();
+    driver.findElement(By.id("Email")).clear();
+    driver.findElement(By.id("Email")).sendKeys("prueba123");
+    driver.findElement(By.id("Password")).click();
+    driver.findElement(By.id("Password")).clear();
+    driver.findElement(By.id("Password")).sendKeys("novan");
+    driver.findElement(By.id("ConfirmPassword")).click();
+    driver.findElement(By.id("ConfirmPassword")).clear();
+    driver.findElement(By.id("ConfirmPassword")).sendKeys("acoincidir");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    driver.findElement(By.linkText("Register")).click();
+    driver.findElement(By.id("Email")).click();
+    driver.findElement(By.id("Email")).clear();
+    driver.findElement(By.id("Email")).sendKeys("prueba123@dominio.uig");
+    driver.findElement(By.id("Password")).click();
+    driver.findElement(By.id("Password")).clear();
+    driver.findElement(By.id("Password")).sendKeys("corta");
+    driver.findElement(By.id("ConfirmPassword")).click();
+    driver.findElement(By.id("ConfirmPassword")).clear();
+    driver.findElement(By.id("ConfirmPassword")).sendKeys("corta");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    driver.findElement(By.linkText("Register")).click();
+    driver.findElement(By.id("Email")).click();
+    driver.findElement(By.id("Email")).clear();
+    driver.findElement(By.id("Email")).sendKeys("dominionovalido");
+    driver.findElement(By.id("Password")).click();
+    driver.findElement(By.id("Password")).clear();
+    driver.findElement(By.id("Password")).sendKeys("ABC123");
+    driver.findElement(By.id("ConfirmPassword")).click();
+    driver.findElement(By.id("ConfirmPassword")).clear();
+    driver.findElement(By.id("ConfirmPassword")).sendKeys("ABC123");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    driver.findElement(By.linkText("Register")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
