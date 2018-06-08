b88478ebc06deb0a30d411b0da515baabe351ec9
Kevin Carracedo Vázquez <kevirgitano@gmail.com>
2018-06-09 00:29:23 +0200

    HtmlUnitDriver

--- Sesion06kcv239/test/ual/hmis/sesion06/RegistroRandom.java
+++ Sesion06kcv239/test/ual/hmis/sesion06/RegistroRandom.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class RegistroRandom {
@@ -17,7 +18,8 @@ public class RegistroRandom {
 
   @Before
   public void setUp() throws Exception {
-    driver = new FirefoxDriver();
+	driver = new HtmlUnitDriver();
+    //driver = new FirefoxDriver();
     baseUrl = "https://www.katalon.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
